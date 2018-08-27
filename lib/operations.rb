79525f8d3e15557ac89cb829bd303017b1c867e2
Absalom Cherinet <absalomcherinet@gmail.com>
2018-08-27 17:56:18 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,13 @@
 def unsafe?(speed)
 
-if speed > 60
+	if(speed <60 && speed > 40)
+		false
+elsif speed > 60 || speed < 40
 	true
-elsif speed < 40
-	true
-elsif(speed <60 && speed > 40)
-	false
+
 end
 
 def not_safe?(speed)
 	(speed <60 && speed > 40) ? false : ((speed > 60? true : (speed < 40 ? true : false)))
-
+ speed < 40 || speed > 60 ? true : false
 end
