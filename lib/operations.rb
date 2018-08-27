eb2b6afc951b154fc7c37bdbbc3ee02ff3cc3392
Absalom Cherinet <absalomcherinet@gmail.com>
2018-08-27 17:53:51 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
 
+if speed > 60
+	true
+elsif speed < 40
+	true
+elsif(speed <60 && speed > 40)
+	false
 end
-
-
 
 def not_safe?(speed)
-	
+	(speed <60 && speed > 40) ? false : ((speed > 60? true : (speed < 40 ? true : false))
+
 end
-	
-
-
