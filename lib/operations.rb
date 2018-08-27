2d27a721c0f7664d3b9da8d5a5103f8a2cb5347b
Absalom Cherinet <absalomcherinet@gmail.com>
2018-08-27 17:54:38 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,6 +9,6 @@ elsif(speed <60 && speed > 40)
 end
 
 def not_safe?(speed)
-	(speed <60 && speed > 40) ? false : ((speed > 60? true : (speed < 40 ? true : false))
+	(speed <60 && speed > 40) ? false : ((speed > 60? true : (speed < 40 ? true : false)))
 
 end
